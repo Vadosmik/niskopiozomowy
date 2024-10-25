671bbe8702d8d9a1aaa7334489391d85313fe8fd
Vadosmik <wadimmikanowicz@gmail.com>
2024-10-25 13:41:05 +0200

    po zajeciach

--- c_project_1/main.c
+++ c_project_1/main.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 char str1[] = "^^Hel1o*_W0rld$";
 
-/*
-void convertDigitToWord(char ch)
+char *conn_string(char *str3, char *str2)
 {
+  uint16_t len1 = strlen(str3);
+  uint16_t len2 = strlen(str2);
+  char *str = (char *)malloc(len1 + len2 + 1);
+  memcpy(str, str3, len1);
+  memcpy(str + len1, str2, len2);
+  str[len1 + len1] = 0;
+
+  return str;
+}
+
+/* nie wiem jak 
+char *convertDigitToWord(char ch)
+{
+  char *out;
   switch (ch)
   {
-  case 0:
-    return 'zero';
-  case 1:
-    return 'one';
-  case 2:
-    return 'two';
-  case 3:
-    return 'three';
-  case 4:
-    return 'four';
-  case 5:
-    return 'five';
-  case 6:
-    return 'six';
-  case 7:
-    return 'seven';
-  case 8:
-    return 'eight';
-  case 9:
-    return 'nine';
+  case 48:
+    out = "zero";
+    break;
+  case 49:
+    out = "one";
+    break;
+  case 50:
+    out = "two";
+    break;
   default:
     break;
   }
+  return out;
 }
 */
 
@@ -48,17 +54,21 @@ char processChar(char ch, int change_case)
 
 char *replaceFnc(const char *input_str, char sep, int change_case)
 {
-  static char answer[100];
+  static char answer[sizeof(str1)];
   int is_separator_needed = 0;
   int i = 0;
 
   while(*input_str)
   {
-    if ((*input_str >= 48 && *input_str <= 57) || (*input_str >= 65 && *input_str <= 90) || (*input_str >= 97 && *input_str <= 122))
+    if ((*input_str >= 65 && *input_str <= 90) || (*input_str >= 97 && *input_str <= 122))
     {
       answer[i++] = processChar(*input_str, change_case);
       is_separator_needed = 1;
     }
+    else if (*input_str >= 48 && *input_str <= 57) 
+    {
+      answer[i++] = *input_str;
+    } 
     else
     {
       if (is_separator_needed)
--- c_project_2/main.c
+++ c_project_2/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
+// static char str1[] = "Hello1";
+// char *str2 = "Hello2";
 
-int main(void) {
-  
-  printf("Hello World");
+char *conn_string(char *str1, char *str2)
+{
+  uint16_t len1 = strlen(str1);
+  uint16_t len2 = strlen(str2);
+  char *str = (char *)malloc(len1 + len2 + 1);
+  memcpy(str, str1, len1);
+  memcpy(str + len1, str2, len2);
+  str[len1 + len1] = 0;
 
+  // uint16_t i = 0;
+  // while(*str1)
+  // {
+  //   str[i] = *str1;
+  //   str1++;
+  //   i++;
+  // }
+  // while(*str2)
+  // {
+  //   str[i] = *str2;
+  //   str2++;
+  //   i++;
+  // }
+  // str[i] = '\0';
+  return str;
+}
+
+int main(void)
+{
+  char *str_a = "connect";
+  char *str_b = " string";
+  char *str_out = conn_string(str_a, str_b);
+  printf("%s", str_out);
   return 0;
 }
